06f155886b47225597a6e02682d2b956e59c2a38
TechAbraao <abraao.cutielips@hotmail.com>
2025-07-15 19:38:05 -0300

    feat: adding submodule templates

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,7 +4,7 @@ from src.app.blueprints.api.decks import decks
 from src.app.blueprints.api.flashcards import flashcards
 
 def create_app() -> Flask:
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates")
     
     app.register_blueprint(decks)
     app.register_blueprint(flashcards)  
